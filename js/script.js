2e01a43de811a48889fb2e1cd35ae1cc17b6106e
Natali <natasha20ka@gmail.com>
2023-04-04 22:05:05 +0300

    мой первый коммит

---
+++ js/script.js
@@ -0,0 +1,33 @@
+console.log("Хомяк ест морковь");
+
+{
+let ham = 15;
+let ster = "Хомяшка красотун";
+const cat = "а Кот нет";
+console.log(ham+" "+ster+", "+cat);
+};
+
+let anfisaKisa = {
+       name: "Anfisa",
+       age: 17,
+       color: "brown",
+       socks: "white"
+};
+
+let family = ["Big Cat", "Small Hamster", anfisaKisa, "love"];
+console.log(family[1]);
+console.log(anfisaKisa.name);
+
+//alert("HamsterSexy");
+
+//const a = confirm("Я сегодня отсосу кыту?");
+//console.log(a);
+
+//const b = prompt("Кыт красивый?", "Очень!");
+//console.log(b);
+
+const c = 15;
+const x = prompt('Сколько у Кыта лапок?', "");
+console.log(x);
+
+console.log(c>x);
