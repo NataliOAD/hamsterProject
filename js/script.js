a51a549e2ecbbcaf14e990dbbfbf1363878a2f7e
Natali <natasha20ka@gmail.com>
2023-04-09 19:53:26 +0300

    шаг 1

--- js/script.js
+++ js/script.js
@@ -1,33 +1,22 @@
-console.log("Хомяк ест морковь");
+"use strict";
 
-{
-let ham = 15;
-let ster = "Хомяшка красотун";
-const cat = "а Кот нет";
-console.log(ham+" "+ster+", "+cat);
+let numberOfFilms = prompt("Сколько фильмов Вы уже посмотрели?", "");
+console.log(numberOfFilms);
+
+let a = prompt("Один из последних просмотренных фильмов?", "");
+let b = prompt("На сколько оцените его?", "");
+let c = prompt("Один из последних просмотренных фильмов?", "");
+let d = prompt("На сколько оцените его?", "");
+
+console.log(a, b);
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {
+        a, b, c, d
+    },
+    actors: {},
+    genres: [],
+    privat: false
 };
-
-let anfisaKisa = {
-       name: "Anfisa",
-       age: 82,
-       color: "brown",
-       socks: "white"
-};
-
-let family = ["Big Cat", "Small Hamster", anfisaKisa, "love"];
-console.log(family[1]);
-console.log(anfisaKisa.name);
-
-//alert("HamsterSexy");
-
-//const a = confirm("Я сегодня отсосу кыту?");
-//console.log(a);
-
-//const b = prompt("Кыт красивый?", "Очень!");
-//console.log(b);
-
-const c = 15;
-const x = prompt('Сколько у Кыта лапок?', "");
-console.log(x);
-
-console.log(c>x);
+console.log(personalMovieDB);
